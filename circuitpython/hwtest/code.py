629109754147c222690b6e4046d4d13294bc2409
Tod Kurt <todbot@users.noreply.github.com>
2023-08-22 15:33:25 -0700

    sync up

--- circuitpython/hwtest/code.py
+++ circuitpython/hwtest/code.py
@@ -1,13 +1,10 @@
 print("Hello World!")
 
-
 import time, board, busio
 import audiomixer, synthio, audiopwmio
 import neopixel
 import touchio
-from adafruit_debouncer import Debouncer
-
-
+from adafruit_debouncer import Debouncer, Button
 
 SAMPLE_RATE=28000
 
@@ -26,14 +23,27 @@ touch_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5,
 
 audio = audiopwmio.PWMAudioOut(pwm_audio_pin)
 mixer = audiomixer.Mixer(voice_count=1, sample_rate=SAMPLE_RATE, channel_count=1,
-                         bits_per_sample=16, samples_signed=True, buffer_size=2048 ) # buffer_size=4096 )
+                         bits_per_sample=16, samples_signed=True, buffer_size=2048 )
 audio.play(mixer)
 
 uart = busio.UART(rx=uart_rx_pin, tx=uart_tx_pin, baudrate=31250, timeout=0.001)
 
 leds = neopixel.NeoPixel(neopixel_pin, num_leds, brightness=0.1)
 
+touch_threshold_adjust = 100
+touch_ins = []  # for debug
+touch_pads = []
+for pin in touch_pins:
+    touchin = touchio.TouchIn(pin)
+    touchin.threshold += touch_threshold_adjust
+    touch_pads.append( Button(touchin, value_when_pressed=True))
+    touch_ins.append(touchin)  # for debug
+num_touch_pads = len(touch_pads)
 
+print("\n----------")
+print("picotouch hwtest hello")
 while True:
-    print("hi")
-    time.sleep(1)
+    for i in range(num_touch_pads):
+        print("%d  " % touch_ins[i].value, end='')
+    print()
+    time.sleep(0.01)
